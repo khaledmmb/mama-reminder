3236abea74b4d4257783c5aca19007e9f7b095fa
khaledmmb <58882823+khaledmmb@users.noreply.github.com>
2026-04-17 01:11:16 +0200

    Add audio-config.js for mapping tips to audio files

---
+++ audio-config.js
@@ -0,0 +1,13 @@
+const audioConfig = {
+  pregnancyTips: {
+    tip1: 'audio/نصيحة_1.mp3',
+    tip2: 'audio/نصيحة_2.mp3',
+    tip3: 'audio/نصيحة_3.mp3'
+  },
+  breastfeedingTips: {
+    tip1: 'audio/نصيحة_4.mp3',
+    tip2: 'audio/نصيحة_5.mp3'
+  }
+};
+
+export default audioConfig;
